978b0fc03f38ea7b3d879814c2db3b6bf81dc300
William Tsoi <william@williamtsoi.net>
2021-07-21 14:40:48 +0800

    fix launch script

--- launch.sh
+++ launch.sh
@@ -4,16 +4,16 @@
 export TF_VAR_PROJECT=$(gcloud config get-value project)
 export TF_VAR_REGION="us-central1"
 export PROJECT_ID=$(gcloud config get-value project)
-export serviceAccount="tfe-deployment-sa@"$PROJECT_ID".iam.gserviceaccount.com"
 
 # Install Terraform
-sudo gsutil cp gs://tfe-resources/wget /usr/bin/wget
-sudo gsutil cp gs://tfe-resources/unzip /usr/bin/unzip
-sudo chmod 755 /usr/bin/unzip
-sudo chmod 755 /usr/bin/wget
-sudo wget https://releases.hashicorp.com/terraform/1.0.2/terraform_1.0.2_linux_amd64.zip
-sudo unzip terraform_1.0.2_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
+# sudo gsutil cp gs://tfe-resources/wget /usr/bin/wget
+# sudo gsutil cp gs://tfe-resources/unzip /usr/bin/unzip
+# sudo chmod 755 /usr/bin/unzip
+# sudo chmod 755 /usr/bin/wget
+mkdir -p ~/bin
+sudo wget https://releases.hashicorp.com/terraform/1.0.2/terraform_1.0.2_linux_amd64.zip -O ~/bin/terraform.zip
+sudo unzip ~/bin/terraform.zip -d ~/bin
+sudo mv ~/bin/terraform /usr/local/bin/
 
 # Terraform Apply & Run
 cd terraform
